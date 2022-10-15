e0c88a2c974e2932e2038d22cf7b8188be99dd3c
krylowicz <kacper@krylowicz.pl>
2022-10-15 12:30:54 +0200

    read cli flags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+const HELP: &str = "
+USAGE:
+    tinyvalue [FLAGS]
+
+FLAGS:
+    -c, --config     Config file path
+";
+
+struct Shard {
+    name: String,
+    index: i32,
+    address: String,
+    config: std::path::PathBuf,
+    shard: String,
+    replica: String
+}
+
+fn parse_flags(args: &Vec<String>) -> Result<&str, ()> {
+    match args.len() {
+        2 => {
+            Ok(args[1].split("=").collect::<Vec<&str>>()[1])
+        }
+        _ => {
+            println!("{HELP}");
+            Err(())
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let config_path = parse_flags(&args).unwrap();
 }
