9210aaa042f8a9296f6663b1347fc63ef3c7bee6
krylowicz <kacper@krylowicz.pl>
2022-10-16 14:48:41 +0200

    returning http response

--- src/server.rs
+++ src/server.rs
@@ -3,15 +3,39 @@ use std::{
     net::TcpStream,
 };
 
+enum Status {
+    Ok,
+    NotFound,
+}
+
+fn create_response(message: &str, status: Status) -> String {
+    let mut response = String::new();
+
+    match status {
+        Status::Ok => {
+            response.push_str("HTTP/1.1 200 OK\r\n");
+        }
+        Status::NotFound => {
+            response.push_str("HTTP/1.1 404 NOT FOUND\r\n");
+        }
+    }
+
+    let length = message.len();
+    let contents = format!("Content-Length: {length}\r\n\r\n{message}");
+    response.push_str(&contents);
+
+    response
+}
+
 pub fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     
-    let endpoint = match &request_line[..] {
-        "GET /get HTTP/1.1" => "/get",
-        "GET /set HTTP/1.1" => "/set",
-        _ => ""
+    let response = match &request_line[..] {
+        "GET /get HTTP/1.1" => create_response("/get", Status::Ok),
+        "GET /set HTTP/1.1" => create_response("/set", Status::Ok),
+        _ => create_response("endpoint is either /get or /set", Status::NotFound)
     };
 
-    println!("{endpoint}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
